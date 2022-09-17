0d82b3f0c1f55ee2427db2496312dcb75f924636
Regina <80333829+Regina-alt@users.noreply.github.com>
2022-09-17 14:52:51 +0300

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,25 @@
+<?php
+echo $_POST['text'];
+echo "</br>";
+
+echo $_POST['password'];
+echo "</br>";
+
+echo $_POST['check1'];
+echo $_POST['check2'];
+echo $_POST['check3'];
+echo "</br>";
+
+echo $_POST['radio'];
+echo "</br>";
+
+echo $_POST['textarea'];
+echo "</br>";
+
+echo $_POST['select'];
+echo "</br>";
+
+echo $_POST['select_double'];
+echo "</br>";
+
+?>
